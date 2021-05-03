580362ed12ed1f5f640efb8d392ad1b72c1c14b0
amagalhaes <alexandremagalhaes31@gmail.com>
2021-05-03 19:58:18 -0300

    DHT Sensor is working.

--- esp/main/sensorDHT/sensorDHT.c
+++ esp/main/sensorDHT/sensorDHT.c
@@ -5,7 +5,46 @@
  *      Author: Alexandre Magalhães
  */
 
+#include <stdio.h>
+#include "freertos/FreeRTOSConfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "sensorDHT.h"
+#include "dht.h"
+#include "../hardwareDefinitions.h"
+#include "../debug.h"
 
-void inicializaSensorDHT (void);
+
+static const dht_sensor_type_t sensor_type = DHT_TYPE_DHT11;
+DHT_MEDIDAS dhtMedida;
+
+
+DHT_MEDIDAS getDHTMedidas (void) {
+	return dhtMedida;
+}
+
+void leituraSensorDHT (void *pvParameters) {
+
+    gpio_set_pull_mode(SENSOR_DHT, GPIO_PULLUP_ONLY);
+
+    while (1)
+    {
+        if (dht_read_data(sensor_type, SENSOR_DHT, &dhtMedida.umidade, &dhtMedida.temperatura) == ESP_OK) {
+        	if (DEBUG_DHT)
+        		printf("Umidade: %d%% Temperatura: %dC\n", dhtMedida.umidade / 10, dhtMedida.temperatura / 10);
+        }
+        else {
+        	if (DEBUG_DHT)
+        		printf("Falha na leitura do sensor DHT11 \n");
+        }
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+
+}
+
+void inicializaSensorDHT (void) {
+
+	xTaskCreate(leituraSensorDHT, "leituraSensorDHT", configMINIMAL_STACK_SIZE * 3, NULL, 2, NULL);
+}
 
--- esp/main/sensorDHT/sensorDHT.h
+++ esp/main/sensorDHT/sensorDHT.h
@@ -8,8 +8,13 @@
 #ifndef MAIN_SENSORDHT_SENSORDHT_H_
 #define MAIN_SENSORDHT_SENSORDHT_H_
 
-void inicializaSensorDHT (void){
+typedef struct {
+    int16_t umidade;
+    int16_t temperatura;
+} DHT_MEDIDAS;
 
-}
+DHT_MEDIDAS getDHTMedidas (void);
+void leituraSensorDHT (void *pvParameters);
+void inicializaSensorDHT (void);
 
 #endif /* MAIN_SENSORDHT_SENSORDHT_H_ */
