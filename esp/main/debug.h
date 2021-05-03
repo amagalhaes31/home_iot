392e33e5d5bc6b8d336ca92e45343874d58b3a9c
amagalhaes <alexandremagalhaes31@gmail.com>
2021-05-03 19:56:48 -0300

    Add the debug.h file.

---
+++ esp/main/debug.h
@@ -0,0 +1,16 @@
+/*
+ * debug.h
+ *
+ *  Created on: 3 de mai. de 2021
+ *      Author: Alexandre Magalhães
+ */
+
+#ifndef MAIN_DEBUG_H_
+#define MAIN_DEBUG_H_
+
+// 1: Habilita o debug
+// 0: Desabilita o debug
+
+#define DEBUG_DHT	1
+
+#endif /* MAIN_DEBUG_H_ */
