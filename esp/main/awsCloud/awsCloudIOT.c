313337f4d342aa8a946c9f750fb9354f497d87ef
amagalhaes <alexandremagalhaes31@gmail.com>
2021-05-13 19:45:10 -0300

    Create the aws folder.

---
+++ esp/main/awsCloud/awsCloudIOT.c
@@ -0,0 +1,8 @@
+/*
+ * awsCloudIOT.c
+ *
+ *  Created on: 13 de mai. de 2021
+ *      Author: Alexandre Magalhães
+ */
+
+
---
+++ esp/main/awsCloud/awsCloudIOT.h
@@ -0,0 +1,13 @@
+/*
+ * awsCloudIOT.h
+ *
+ *  Created on: 13 de mai. de 2021
+ *      Author: Alexandre Magalhães
+ */
+
+#ifndef MAIN_AWSCLOUD_AWSCLOUDIOT_H_
+#define MAIN_AWSCLOUD_AWSCLOUDIOT_H_
+
+
+
+#endif /* MAIN_AWSCLOUD_AWSCLOUDIOT_H_ */
